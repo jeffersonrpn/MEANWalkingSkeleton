a352bb7c52665a5358b7b5d7051019918df8e71f
Jefferson Neves <jefferson.rpn@gmail.com>
2015-06-19 18:05:50 -0300

    Setup of MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-	stylus = require('stylus');
+	stylus = require('stylus'),
+	mongoose = require('mongoose');
 
 // Sets the enviroment of Node
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -22,12 +23,29 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+
+// Persistence
+mongoose.connect('mongodb://localhost/meanstack');
+var db = mongoose.connection;
+db.on('errors', console.error.bind(console, 'connection error...'));
+db.on('open', function callback() {
+	console.log('meanstack db opened');
+});
+var messageSchema = mongoose.Schema({ message : String });
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(error, messageDoc){
+	mongoMessage = messageDoc.message;
+});
+
 // Routes
 app.get('/partials/:path', function (request, response) {
 	response.render('partials/' + request.params.path);
 });
 app.get('*', function (request, response) {
-	response.render('index');
+	response.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 var port = 3030;
